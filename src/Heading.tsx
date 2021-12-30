9e7dbc3f9d49a7696aa3b59bf70d9be1a0a4214b
Venkatesh <venkatesh.kamalapathy@analog.com>
2021-12-31 03:30:47 +0530

    Added css to the header

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import './Heading.css';
 export type HeadingProp = { title: string };
 
 export function Heading({ title }: HeadingProp) {
-    return <h1>{title}</h1>;
+    return <h1 className="header glow">{title}</h1>;
 }
