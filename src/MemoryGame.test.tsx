8e06f2911c17f536003d84b5bf9166f35ae369f8
Venkatesh <venkatesh.kamalapathy@analog.com>
2021-12-31 18:11:18 +0530

    Adding score's child element - red

--- src/MemoryGame.test.tsx
+++ src/MemoryGame.test.tsx
@@ -11,3 +11,21 @@ test('MemoryGame should contain score class', () => {
     const { container } = render(<MemoryGame />);
     expect(container.querySelector('.score')).toBeInTheDocument();
 });
+
+test('MemoryGame should contain Matches label', () => {
+    const { container } = render(<MemoryGame />);
+    const matchesTextElement = screen.getByText(/matches/i);
+    expect(matchesTextElement).toBeInTheDocument();
+});
+
+test('MemoryGame should contain Attempts label', () => {
+    const { container } = render(<MemoryGame />);
+    const attemptsTextElement = screen.getByText(/attempts/i);
+    expect(attemptsTextElement).toBeInTheDocument();
+});
+
+test('MemoryGame should contain Attempts element', () => {
+    const { container } = render(<MemoryGame />);
+    const attemptsTextElement = screen.getByTestId('attempts');
+    expect(attemptsTextElement).toBeInTheDocument();
+});
--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export function MemoryGame() {
     return (
         <>
+            <div className="score"></div>
             <div className="grid"></div>
         </>
     );
