b2a3afe1fb7edb8915545f403222d20098dad2d2
Venkatesh <venkatesh.kamalapathy@analog.com>
2021-12-31 18:21:13 +0530

    Adding score's child element - refactor

--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
+export function Score() {
+    return (
+        <div className="score">
+            <h1>
+                Matches:
+                <span id="matches" data-testid="matches">
+                    0
+                </span>
+            </h1>
+            <h1>
+                Attempts:
+                <span id="attempts" data-testid="attempts">
+                    0
+                </span>
+            </h1>
+        </div>
+    );
+}
+
 export function MemoryGame() {
     return (
         <>
-            <div className="score">
-                <h1>
-                    Matches:
-                    <span id="matches" data-testid="matches">
-                        0
-                    </span>
-                    Attempts:
-                    <span id="attempts" data-testid="attempts">
-                        0
-                    </span>
-                </h1>
-            </div>
+            <Score />
             <div className="grid"></div>
         </>
     );
