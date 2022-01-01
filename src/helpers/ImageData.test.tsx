a2a16d9117ee4d748115358c0d9fce1063c9b44e
Venkatesh <venkatesh.kamalapathy@analog.com>
2022-01-02 03:36:33 +0530

    ImageData must be shuffled - Red

--- src/helpers/ImageData.test.tsx
+++ src/helpers/ImageData.test.tsx
@@ -4,3 +4,13 @@ test('Should return ImageData with 16 items', () => {
     let result = getImageData();
     expect(result.length).toEqual(16);
 });
+
+test('Should return ImageData after shuffling', () => {
+    let result1 = getImageData();
+    let result2 = getImageData();
+    let shuffled = false;
+    result1.map((data, i) => {
+        if (result2[i].name !== data.name) shuffled = true;
+    });
+    expect(shuffled).toBeTruthy();
+});
