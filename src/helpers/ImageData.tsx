46d4210123ae5962e1f8cb034d50237fcf8e04bb
Venkatesh <venkatesh.kamalapathy@analog.com>
2022-01-02 03:36:55 +0530

    ImageData must be shuffled - Green

--- src/helpers/ImageData.tsx
+++ src/helpers/ImageData.tsx
@@ -46,9 +46,19 @@ export function getImageData(): ImageData[] {
     ];
 
     data = duplicateImageData(data);
+    shuffle(data);
     return data;
 }
 
 function duplicateImageData(data: ImageData[]) {
     return data.concat(data);
+}
+
+function shuffle(array: ImageData[], len = array.length) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array.slice(0, len);
 }
